09d8c6e735e66d793e62658521ba4793acf2cc46
Matéo Fernandez <mateo.fernandez73@gmail.com>
2025-03-26 13:01:49 +0100

    feat(scala-spark): add spark-operator + minio to run our jobs

--- scala-spark/src/main/scala/SimpleApp.scala
+++ scala-spark/src/main/scala/SimpleApp.scala
@@ -1,15 +1,28 @@
 package fr.umontpellier.polytech
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkFiles
+import org.apache.spark.SparkContext
 
 object SimpleApp {
   def main(args: Array[String]): Unit = {
-    val logFile = "data/README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
+    val spark =
+      SparkSession
+        .builder()
+        .appName("Simple Application")
+        .master("local[*]")
+        .getOrCreate()
+
+    // println("Listing files in the SparkContext:")
+    // SparkContext.getOrCreate().listFiles().foreach(println)
+    // println()
+
+    val logData = spark.read.textFile(f"file:///${SparkFiles.get("README.md")}")
+
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
+
     spark.stop()
   }
 }
